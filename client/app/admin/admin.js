27bce516a1f9b2106bbfafbb81bba86a9c37cf39
Eli Sagy <sagyeli@gmail.com>
2015-06-29 20:33:58 +0000

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('themealzApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
