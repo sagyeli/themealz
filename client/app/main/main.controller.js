cdca55f05b7e7dd7a5f55f4f0948f0b8516164ec
Eli Sagy <sagyeli@gmail.com>
2015-09-16 12:21:30 +0000

    Allowing to set and delete meal options (title, info and activation status) from client side

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,4 +2,29 @@
 
 angular.module('themealzApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.mealOptions = [];
+    $scope.newMealOptionActive = true;
+
+    $http.get('/api/mealOptions').success(function(mealOptions) {
+      $scope.mealOptions = mealOptions;
+      socket.syncUpdates('mealOption', $scope.mealOptions);
+    });
+
+    $scope.addMealOption = function() {
+      if($scope.newMealOptionTitle === '') {
+        return;
+      }
+      $http.post('/api/mealOptions', { name: $scope.newMealOptionTitle, info: $scope.newMealOptionInfo, active: $scope.newMealOptionActive });
+      $scope.newMealOptionTitle = '';
+      $scope.newMealOptionInfo = '';
+      $scope.newMealOptionActive = true;
+    };
+
+    $scope.deleteMealOption = function(mealOption) {
+      $http.delete('/api/mealOptions/' + mealOption._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('mealOption');
+    });
   });
